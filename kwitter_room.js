86628926c955364c8004db64dfa5d055b9843206
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-02-20 16:37:35 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBPBjvbO8qdrzss_0RncYYgYPTtHnC8jbE",
+    authDomain: "kwitter-project-7c728.firebaseapp.com",
+    databaseURL: "https://kwitter-project-7c728-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-7c728",
+    storageBucket: "kwitter-project-7c728.appspot.com",
+    messagingSenderId: "1001453503646",
+    appId: "1:1001453503646:web:b1aa094feee2ded540278f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
